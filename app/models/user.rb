e54df63af525969f24a4bdabe301cca6d2ee3cfc
Christian Villalpando Márquez <cristiano.011@hotmail.com>
2016-06-30 20:13:27 -0500

    Septimo Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
 	validates :name, :lastname, presence: true
 	validates :username, :email, presence: true, uniqueness: true
 
+	has_many :lists, dependent: :destroy
+
 end
